fed758b35adeec266662c642d3c73be19a0c1cb3
Brian Lovell <brian.lovell@outlook.com>
2022-05-23 11:03:49 -0400

    [Add] udacity example API call

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 console.log(process.env) // Remove after testing
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 const port = 3000
@@ -18,10 +19,25 @@ app.use(function (req, res) {
     res.end(JSON.stringify(req.body, null, 2))
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+app.use('/', express.static(path.join(__dirname, '../public')))
+
+// example API call
+app.get('/apod', async (req, res) => {
+    try {
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
