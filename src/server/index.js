76febba3e9c99ce0b92ea7c774f92261815c8ff3
Brian Lovell <brian.lovell@outlook.com>
2022-05-23 11:24:35 -0400

    [Add] node fetch

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ console.log(process.env) // Remove after testing
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const fetch = require('node-fetch')
 
 const app = express()
 const port = 3000
@@ -13,14 +14,25 @@ app.use(bodyParser.urlencoded({ extend: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-})
+// app.use(function (req, res) {
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.write('you posted:\n')
+//     res.end(JSON.stringify(req.body, null, 2))
+// })
 
 app.listen(port, () => {
     console.log(`Server app listening on port ${port}`)
 })
 
 app.use('/', express.static(path.join(__dirname, '../public')))
+
+// example API call
+app.get('/apod', async (req, res) => {
+    try {
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ image })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
