56afd6f5c728e9200ae256ecf3da4d8908519c88
Brian Lovell <brian.lovell@outlook.com>
2022-05-23 13:27:00 -0400

    [Update] generated page layout

--- src/public/client.js
+++ src/public/client.js
@@ -35,16 +35,7 @@ const App = (state) => {
         <main>
             ${Greeting(store.user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
+                <h2>Rover: Insert Rover Name here Brian</h2>
                 ${ImageOfTheDay(apod)}
             </section>
         </main>
