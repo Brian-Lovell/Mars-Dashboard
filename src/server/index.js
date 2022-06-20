1cb7ebd1953c3546de4b14dc458cc0e60bfd32b5
Brian Lovell <brian.lovell@outlook.com>
2022-06-20 15:55:58 -0400

    [Create] API call for mission manifests

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,9 @@ const fetch = require('node-fetch')
 const app = express()
 const port = 3000
 
+// API URL
+const mmURL = 'https://api.nasa.gov/mars-photos/api/v1/manifests/'
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extend: false }))
 
@@ -26,7 +29,7 @@ app.listen(port, () => {
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// example API call
+// Astronomy Picture of the Day API call
 app.get('/apod', async (req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
@@ -34,5 +37,16 @@ app.get('/apod', async (req, res) => {
         res.send({ image })
     } catch (err) {
         console.log('error:', err);
+    }
+})
+
+// Mars Rover Photos Mission Manifest API call
+app.get('/mm-curiosity', async (req, res) => {
+    try {
+        let mission = await fetch(`${mmURL}curiosity?&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ mission })
+    } catch (err) {
+        console.log('error', err);
     }
 })
