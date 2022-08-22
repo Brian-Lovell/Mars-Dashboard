85d347eb33cfd3f6609fb8526e4eb24bd1099a5a
Brian Lovell <brian.lovell@outlook.com>
2022-08-22 15:49:54 -0400

    [Modify] variables to use const

--- src/server/index.js
+++ src/server/index.js
@@ -32,7 +32,7 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // Astronomy Picture of the Day API route
 app.get('/apod', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        const image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
@@ -43,7 +43,7 @@ app.get('/apod', async (req, res) => {
 // Mars Rover Mission Manifest API route
 app.get('/mission-curiosity', async (req, res) => {
     try {
-        let mission = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?&api_key=${process.env.API_KEY}`)
+        const mission = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?&api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ mission })
     } catch (err) {
@@ -54,7 +54,7 @@ app.get('/mission-curiosity', async (req, res) => {
 // Mar Rover Phtos API route
 app.get('/photos-curiosity', async (req, res) => {
     try {
-        let photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=${process.env.API_KEY}`)
+        const photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ photos })
     } catch (err) {
@@ -66,7 +66,7 @@ app.get('/photos-curiosity', async (req, res) => {
 // Mars Rover Mission Manifest API route
 app.get('/mission-opportunity', async (req, res) => {
     try {
-        let mission = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity?&api_key=${process.env.API_KEY}`)
+        const mission = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity?&api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ mission })
     } catch (err) {
@@ -77,7 +77,7 @@ app.get('/mission-opportunity', async (req, res) => {
 // Mar Rover Phtos API route
 app.get('/photos-opportunity', async (req, res) => {
     try {
-        let photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/latest_photos?api_key=${process.env.API_KEY}`)
+        const photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/latest_photos?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ photos })
     } catch (err) {
@@ -89,7 +89,7 @@ app.get('/photos-opportunity', async (req, res) => {
 // Mars Rover Mission Manifest API route
 app.get('/mission-spirit', async (req, res) => {
     try {
-        let mission = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/spirit?&api_key=${process.env.API_KEY}`)
+        const mission = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/spirit?&api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ mission })
     } catch (err) {
@@ -100,7 +100,7 @@ app.get('/mission-spirit', async (req, res) => {
 // Mar Rover Phtos API route
 app.get('/photos-spirit', async (req, res) => {
     try {
-        let photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos?api_key=${process.env.API_KEY}`)
+        const photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ photos })
     } catch (err) {
